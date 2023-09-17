714a902a3eddbc4752dc762958bfbdc920183af1
mbporter <mbporter@email.sc.edu>
2023-09-17 13:54:55 -0400

    adding ass-6 to main page

--- Assignments/Assignment 6/script.js
+++ Assignments/Assignment 6/script.js
@@ -6,8 +6,13 @@ const hideOtter = () => {
     document.getElementById("otter").classList.add("hide");
 }
 
+const moveSquare = () => {
+    document.getElementById("square").classList.add("anna");
+}
+
+
 window.onload = () => {
-    //document.getElementById("button-click").onclick = changeText;
     document.getElementById("button-show").onclick = showOtter;
     document.getElementById("button-hide").onclick = hideOtter;
+    document.getElementById("button-move").onclick = moveSquare;
 }
