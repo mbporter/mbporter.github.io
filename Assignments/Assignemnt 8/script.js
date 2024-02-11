311ff075573f3aa6fbed4a6b3668c1e13b66c482
mbporter <mbporter@email.sc.edu>
2024-02-11 16:21:53 -0500

    mayeb?

--- Assignments/Assignemnt 8/script.js
+++ Assignments/Assignemnt 8/script.js
@@ -2,11 +2,38 @@ const toggleNav = () => {
     document.getElementById("menu-items").classList.toggle("hidden");
 }
 
-const yoga = document.getElementById("yogaslider")
+const CommandLine = document.getElementById("letters");
+const image = document.getElementById("man");
 
+const imgSwitch = () => {
+    const inputValue = CommandLine.value.trim().toLowerCase();
+    let imagePath = 'images/original.jpg';
 
+    const lastChar = inputValue.charAt(inputValue.length - 1);
+    
+    if (lastChar === 'b') {
+        imagePath = 'images/birthday.jpg';
+    } else if (lastChar === 'c') {
+        imagePath = 'images/clown.jpg';
+    } else if (lastChar === 'p') {
+        imagePath = 'images/read.jpg';
+    } else if (lastChar === 'r') {
+        imagePath = 'images/rain.jpg';
+    } else if (lastChar === 's') {
+        imagePath = 'images/shovel.jpg';
+    } else if (lastChar === 'w') {
+        imagePath = 'images/work.jpg';
+    }
 
+    image.src = imagePath;
+};
 
+const changeImage = () => {
+    const sliderValue = document.getElementById("yogaslide").value;
+    const image = document.getElementById("yoga");
+    const imageUrl = "images/yoga" + sliderValue + ".jpg";
+    image.src = imageUrl;
+}
 
 const showOne = () => {
     document.getElementById("exercise-one").classList.remove("hidden");
@@ -18,12 +45,11 @@ const showTwo = () => {
     document.getElementById("exercise-one").classList.add("hidden");
 }
 
-
-
-
 //on load menu
 window.onload = () => {
     document.getElementById("menu-toggle").onclick = toggleNav;
     document.getElementById("eOne").onclick = showOne;
     document.getElementById("eTwo").onclick = showTwo;
-}
+    CommandLine.onkeyup = imgSwitch;
+    document.getElementById("yogaslide").oninput = changeImage;
+};
