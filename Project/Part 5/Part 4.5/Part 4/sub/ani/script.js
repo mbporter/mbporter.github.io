8f21609d54ae208b326d0bbf9622415fd7601533
mbporter <mbporter@email.sc.edu>
2024-03-16 19:01:42 -0400

    throws up eveywher and dies

--- Project/Part 5/Part 4.5/Part 4/sub/ani/script.js
+++ Project/Part 5/Part 4.5/Part 4/sub/ani/script.js
@@ -1,6 +1,27 @@
+const getShoes = async () => {
+ const url = "...........";
+ try {
+    const responce = await fetch(url); 
+    return await responce.json();
+ } catch (error){
+    console.log(error);
+ }
+};
 
 
+const showShoes = async () => {
+    let shoes = await getShoes();
+    let shoesSection = document.getElementById(animal-Section);
+    
+    shoes.forEach(shoe => {
+        shoesSection.apped(getShoeItem(shoe));
+    });
+};
 
-const showShoes = () => {};
+const getShoeItem = shoe => {
+    let section = document.createElement("section");
+
+    return section;
+}
 
 window.onload = () => showShoes ();
